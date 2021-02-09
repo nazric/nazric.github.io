2d0d6c37819430abceb3037c76900f6ddc4e91da
nazric <nazric55@gmail.com>
2021-02-08 20:14:21 -0600

    Drawing red squares :P

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+var canvas;
+var ctx;
+
+i_step = 0;
+function step() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(10 + i_step*10,10 + i_step*10,10,10);
+    i_step++;
+}
+
 window.onload = function() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
 
     ctx.fillStyle = 'green';
-    ctx.fillRect(10, 10, 150, 100);
+    ctx.fillRect(10, 10, 1920, 1080);
+
+    window.setInterval(step, 100);
 };
 
 
