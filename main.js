cfe78980bda2e6be8766dc312a9a9a14c186949c
nazric <nazric55@gmail.com>
2021-02-08 19:44:20 -0600

    Canvas Test

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+document.onload(function() {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10, 10, 150, 100);
+});
