94cd3b1d1570589079aff777128e40b50f930839
nazric <nazric55@gmail.com>
2021-02-14 15:37:47 -0600

    Added some elasticity inputs

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
+// Global Variables
 var canvas;
 var ctx;
 var ball;
 var i_step = 0;
+var elasticity = .99;
+
+// Constants
 const FrameWidth = 1280;
 const FrameHeight = 720;
 const FrameRate = 240;  // fps
-const Elasticity = .99;
 const Background = "#ffe7c9"
+
+// Initial conditions
+var elasticity = .99;
 
 function main() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    // ctx.fillStyle = 'green';
-    // ctx.fillRect(0, 0, 1280, 720);
-
-    // ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    // ctx.fillStyle = 'blue';
-    // ctx.fill();
-
-    ball = {x: 640, y: 50, 'vx':1500, 'vy':0, 'ax':0, 'ay':-1000, rx: 10, ry: 10, shape:'circle', fillStyle: "red"};
-    step();
+    reset();
     window.setInterval(step, 1000 * (1/FrameRate));
+}
+
+function reset() {
+    actors = [];
+    ball = {x: 640, y: 50, 'vx':1500, 'vy':0, 'ax':0, 'ay':-1000, rx: 10, ry: 10, shape:'circle', fillStyle: "red"};
+    actors.push(ball);
+    step();
 }
 
 function step() {
     drawBackground();
-    wallCollission([ball]);
-    updateKin([ball]);
-    renderObjects([ball]);
+    wallCollission(actors);
+    updateKin(actors);
+    renderObjects(actors);
     i_step++;
 }
 
@@ -36,23 +41,24 @@ function drawBackground() {
     ctx.fillStyle = Background;
     ctx.fillRect(0, 0, FrameWidth, FrameHeight);
 }
+
 function wallCollission(objects) {
     objects.forEach(function(value, index, array) {
         if(value.y + value.ry >= FrameHeight) {
             value.y = FrameHeight - value.ry;
-            value.vy *= -Elasticity;
+            value.vy *= -elasticity;
         }
         else if(value.y - value.ry <= 0) {
             value.y = value.rx;
-            value.vy *= -Elasticity;
+            value.vy *= -elasticity;
         }
         if(value.x + value.rx >= FrameWidth){
             value.x = FrameWidth - value.rx;
-            value.vx *= -Elasticity;
+            value.vx *= -elasticity;
         } 
         else if (value.x - value.rx <= 0) {
             value.x = value.rx;
-            value.vx *= -Elasticity;
+            value.vx *= -elasticity;
         }
     });
 }
@@ -80,6 +86,10 @@ function renderObjects(objects) {
             ctx.fill();
         }
     });
+}
+
+function setElasticity(value) {
+    elasticity = value;
 }
 
 window.onload = main;
