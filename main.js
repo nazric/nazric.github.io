88b64a40b94e50859051d3cc593f3ff4c017605b
nazric <43476195+nazric@users.noreply.github.com>
2021-05-08 20:21:07 -0500

    Multiplayer! And physics updates

--- main.js
+++ main.js
@@ -4,19 +4,20 @@ var ctx;
 var ball;
 var i_step = 0;
 var elasticity = .85;
+var HorzCap = 700;
+var VertCap = 700;
 
 // Constants
 const FrameWidth = 1280;
 const FrameHeight = 720;
-const FrameRate = 120;  // fps
+const FrameRate = 60;  // fps
 const Background = "#ffe7c9";
 const Net = "green";
-const Gravity = -1000;
-const HorzCap = 1000;
-const VertCap = 1000;
+const Gravity = -750;
 const NetHeight = 300;
 const NumNetPoly = 300;
 const TopOfNet = {x: FrameWidth/2, y:FrameHeight - NetHeight, 'vx':0, 'vy':0, 'ax':0, 'ay':0, rx: 2, ry: 2, recoil: false, shape:'circle', fillStyle: Net}
+const colourLoop = ["blue", ""];
 
 // Initial conditions
 var elasticity = .63;
@@ -29,20 +30,21 @@ function main() {
     window.addEventListener("keyup", keyUp, false);
 
     reset();
-    window.setInterval(step, 1000 * (1/FrameRate));
+    window.setInterval(step, 1000.0 * (1.0/FrameRate));
 }
 
 function reset() {
     actors = [];
     actors.push();
 
-    ball1 = {x: 640, y: 50, 'vx':1500, 'vy':-1000, 'ax':0, 'ay':Gravity, rx: 40, ry: 40, recoil: false, shape:'circle', fillStyle: "red"};
-    ball2 = {x: 400, y: 50, 'vx':700, 'vy':500, 'ax':0, 'ay':Gravity, rx: 20, ry: 20, recoil: true, shape:'circle', fillStyle: "blue"};
-    actors.push(ball1);
+    player1 = {x: 50, y: FrameHeight-40, newx: -1, newy: -1, 'vx':0, 'vy':0, 'ax':0, 'ay':Gravity, rx: 40, ry: 40, recoil: false, isActor:true, shape:'circle', fillStyle: "#0ba2e3",
+               upPress: false, downPress: false, rightPress: false, leftPress: false};
+    player2 = {x: FrameWidth - 50, y: FrameHeight-40, newx: -1, newy: -1, 'vx':0, 'vy':0, 'ax':0, 'ay':Gravity, rx: 40, ry: 40, recoil: false, isActor:true, shape:'circle', fillStyle: "purple",
+               upPress: false, downPress: false, rightPress: false, leftPress: false};
+    ball2 = {x: 400, y: 50, newx: -1, newy: -1, 'vx':700, 'vy':500, 'ax':0, 'ay':Gravity, rx: 20, ry: 20, recoil: true, shape:'circle', fillStyle: "blue"};
+    actors.push(player1);
+    actors.push(player2);
     actors.push(ball2);
-    for(var i = 0; i < NumNetPoly; i++) {
-        // 
-    }
     // ball3 = {x: 640, y: 200, 'vx':-1500, 'vy':1000, 'ax':0, 'ay':Gravity, rx: 40, ry: 40, recoil: true, shape:'circle', fillStyle: "purple"};
     // ball4 = {x: 100, y: 700, 'vx':-1500, 'vy':-1000, 'ax':0, 'ay':Gravity, rx: 40, ry: 40, recoil: true, shape:'circle', fillStyle: "green"};
     // ball5 = {x: 1000, y: 50, 'vx':1500, 'vy':0, 'ax':0, 'ay':Gravity, rx: 40, ry: 40, recoil: true, shape:'circle', fillStyle: "yellow"};
@@ -59,9 +61,16 @@ function reset() {
     step();
 }
 
+function setSpeedcap(newVal) {
+    HorzCap = newVal;
+    VertCap = newVal;
+}
+
 function step() {
     drawBackground();
     drawNet();
+
+    handleKeys();
     wallCollission(actors);
     objectCollisions(actors);
     netCollision(actors);
@@ -69,60 +78,110 @@ function step() {
     updateKin(actors);
     
     renderObjects(actors);
-    i_step++;
+    // i_step++;
+}
+
+function handleKeys() {
+    handlePlayerInput(player1);
+    handlePlayerInput(player2);
+}
+
+function handlePlayerInput(playerObj) {
+    // left
+    if (playerObj.leftPress) {
+        playerObj.ax = -5000;
+        if(playerObj.vx > -100) {
+            playerObj.vx = -100;
+        }
+    }
+    // right
+    else if(playerObj.rightPress) {
+        playerObj.ax = 5000;
+        if(playerObj.vx < 100) {
+            playerObj.vx = 100;
+        }
+    }
+    else {
+        playerObj.ax = 0;
+    }
+    // up
+    if(playerObj.upPress) {
+        playerObj.ay = 5000;
+        if(playerObj.vy < 100) {
+            playerObj.vy = 100;
+        }
+    }
+    // down
+    else if(playerObj.downPress) {
+        playerObj.ay = -5000 + Gravity;
+        if(playerObj.vy > -100) {
+            playerObj.vy = -100;
+        }
+    }
+    else {
+        playerObj.ay = Gravity;
+    }
 }
 
 function keyDown(event) {
     var key = event.keyCode;
-    // alert(key);
-    // left
-    if (key == 65 || key == 37) {
-        ball1.ax = -5000;
-        if (-ball1.vx >= HorzCap) {
-            ball1.vx = - HorzCap;
-        }
-    }
-    // right
-    else if(key == 68 || key == 39) {
-        ball1.ax = 5000;
-        if (ball1.vx >= HorzCap) {
-            ball1.vx = HorzCap
-        }
-    }
-    // up
-    else if(key == 87 || key == 38) {
-        ball1.ay = 5000;
-        if (ball1.vy >= VertCap) {
-            ball1.vy = VertCap
-        }
-    }
-    // down
-    else if(key == 83 || key == 40) {
-        ball1.ay = -5000;
-
+    switch(key) {
+        case 65:
+            player1.leftPress = true;
+            break;
+        case 37:
+            player2.leftPress = true;
+            break;
+        case 68:
+            player1.rightPress = true;
+            break;
+        case 39:
+            player2.rightPress = true;
+            break;
+        case 87:
+            player1.upPress = true;
+            break;
+        case 38:
+            player2.upPress = true;
+            break;
+        case 83:
+            player1.downPress = true;
+            break;
+        case 40:
+            player2.downPress = true;
+            break;
+        case 82:
+            reset();
     }
 }
 
 function keyUp(event) {
     var key = event.keyCode;
-    // left
-    if (key == 65 || key == 37) {
-        ball1.ax = 0;
-    }
-    // right
-    else if(key == 68 || key == 39) {
-        ball1.ax = 0;
-    }
-    // up
-    else if(key == 87 || key == 38) {
-        ball1.ay = Gravity;
-    }
-    // down
-    else if(key == 83 || key == 40) {
-        ball1.ay = Gravity;
-    }
-    else if(key == 82) {
-        reset();
+    switch(key) {
+        case 65:
+            player1.leftPress = false;
+            break;
+        case 37:
+            player2.leftPress = false;
+            break;
+        case 68:
+            player1.rightPress = false;
+            break;
+        case 39:
+            player2.rightPress = false;
+            break;
+        case 87:
+            player1.upPress = false;
+            break;
+        case 38:
+            player2.upPress = false;
+            break;
+        case 83:
+            player1.downPress = false;
+            break;
+        case 40:
+            player2.downPress = false;
+            break;
     }
 }
 
@@ -136,48 +195,46 @@ function drawNet(){
     ctx.fillRect(FrameWidth/2 - 2, FrameHeight - NetHeight, 4, NetHeight);
 }
 
+function collideNet(object) {
+    if (object.x > FrameWidth / 2) {
+        if (object.x - object.rx < FrameWidth / 2 + 2) {
+            object.x = FrameWidth / 2 + 2 + object.rx;
+            if (object.recoil)
+                object.vx *= -elasticity;
+            else 
+                object.vx = 0;
+        }
+    }
+    else {
+        if(object.x + object.rx > FrameWidth / 2 - 2) {
+            object.x = FrameWidth / 2 - 2 - object.rx;
+            if (object.recoil)
+                object.vx *= -elasticity;
+            else 
+                object.vx = 0;
+        }
+    }  
+}
+
 function netCollision(objects){
     objects.forEach(function(value, index, array) {
-        if(value.y + value.ry > FrameHeight - NetHeight) {
-            if(value.y < FrameHeight - NetHeight) {
-                // Sweetspot
-                if (value.x - value.rx < FrameWidth/2 - 2 && value.x + value.rx > FrameWidth/2 + 2) {
-                    circleCollision(TopOfNet, value);
-                }
-
-            }
-            else {
-                // Below Sweetspot
-                if (value.x > FrameWidth / 2) {
-                    if (value.x - value.rx < FrameWidth / 2 + 2) {
-                        value.x = FrameWidth / 2 + 2 + value.rx;
-                        if (value.recoil)
-                            value.vx *= -elasticity;
-                        else 
-                            value.vx = 0;
-                    }
-                }
-                else {
-                    if(value.x + value.rx > FrameWidth / 2 - 2) {
-                        value.x = FrameWidth / 2 - 2 - value.rx;
-                        if (value.recoil)
-                            value.vx *= -elasticity;
-                        else 
-                            value.vx = 0;
-                    }
-                }                
-            }
+        if(!value.isActor && value.y + value.ry > FrameHeight - NetHeight && value.y < FrameHeight - NetHeight) {
+            circleCollision(TopOfNet, value);
+        }
+        else if(value.isActor || value.y >= FrameHeight - NetHeight) {
+            collideNet(value);
         }
     });
 }   
 
 function goal(rightScored) {
-    if(rightScored) {
-        console.log("right scores!!!!!");
-    }
-    else{
-        console.log("left score!!!!");
-    }
+    // if(rightScored) {
+    //     console.log("right scores!!!!!");
+    // }
+    // else{
+    //     console.log("left score!!!!");
+    // }
+    // reset();
 }
 
 function wallCollission(objects) {
@@ -190,7 +247,6 @@ function wallCollission(objects) {
                 if(value.x > FrameWidth/2) {
                     rightScored = false;
                 }
-                reset();
                 goal(rightScored);
             }
             else if(value.y - value.ry <= 0) {
@@ -248,8 +304,7 @@ function objectCollisions(objects) {
 }
 
 function circleCollision(value1, value2) {
-    // Circle
-    // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+    // Adapted from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
     var dx = value1.x - value2.x;
     var dy = value1.y - value2.y;
     var dist = Math.sqrt(dx * dx + dy * dy);
@@ -260,16 +315,26 @@ function circleCollision(value1, value2) {
         var theta = Math.atan2(dy, dx);
         var xprop = Math.cos(theta);
         var yprop = Math.sin(theta);
-        value2.x = value1.x - xprop * (value1.rx + value2.rx);
-        value2.y = value1.y - yprop * (value1.ry + value2.ry);
+        value1.newx = value2.x + xprop * (value1.rx + value2.rx);
+        value1.newy = value2.y + yprop * (value1.ry + value2.ry);
+        value2.newx = value1.x - xprop * (value1.rx + value2.rx);
+        value2.newy = value1.y - yprop * (value1.rx + value2.rx);
 
         if (!value1.recoil) {
             energy2 = tot_energy;
             energy1 = 0;
+            // value1.ax = 0;
+            // value1.ay = 0;
+            // value1.vx = 0;
+            // value1.vy = 0;
         }
         else if(!value2.recoil) {
             energy1 = tot_energy;
             energy2 = 0;
+            // value2.ax = 0;
+            // value2.ay = 0;
+            // value2.vx = 0;
+            // value2.vy = 0;
         }
         else {
             var energySplit = tot_energy * 0.5;
@@ -290,6 +355,11 @@ function circleCollision(value1, value2) {
 
 function updateKin(objects) {
     objects.forEach(function(value, index, array) {
+        value.x = value.newx >= 0 ? value.newx : value.x;
+        value.y = value.newy >= 0 ? value.newy : value.y;
+        value.newx = -1;
+        value.newy = -1;
+        
         value.vx += value.ax / FrameRate;
         value.vy += value.ay / FrameRate;
         if (Math.abs(value.vy) > VertCap) {
